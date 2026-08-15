ccb6ce407c3ddcc081091e3834c48d29fbbf65c0
Talha-Morshed <professionalxtalha@gmail.com>
2026-08-15 20:07:17 +0600

    Home page design

--- frontend/src/components/landing/Hero.jsx
+++ frontend/src/components/landing/Hero.jsx
@@ -20,22 +20,26 @@ export default function Hero() {
 
           <div className="lg:col-span-6 relative">
             <div className="relative w-full h-96 lg:h-[520px]">
-              <div className="absolute -left-8 top-6 w-36 h-36 md:w-44 md:h-44 transform rotate-3">
+              {/* Top Left - Goat */}
+              <div className="absolute -left-8 -top-12 w-44 h-44 md:w-56 md:h-56 transform rotate-3 z-10">
                 <HeroAnimalCard src={images.goat} alt="Goat" />
               </div>
 
-              <div className="absolute right-8 top-0 w-56 h-56 md:w-72 md:h-72 transform -translate-y-4 shadow-xl">
-                <HeroAnimalCard src={images.sheep} alt="Sheep" />
+              {/* Top Right - Camel */}
+              <div className="absolute -right-6 -top-8 w-56 h-56 md:w-72 md:h-72 transform -translate-y-8 shadow-xl z-10">
+                <HeroAnimalCard src={images.camel} alt="Camel" />
               </div>
 
-              <div className="absolute inset-0 flex items-center justify-center">
+              {/* Center - Cow (Large) - shifted left to avoid overlap */}
+              <div className="absolute -top-12 flex items-center justify-start pl-8 h-full">
                 <div className="w-72 h-72 md:w-96 md:h-96 lg:w-[420px] lg:h-[420px] rounded-3xl overflow-hidden shadow-2xl">
                   <img src={images.cow} alt="Cow" className="w-full h-full object-cover" />
                 </div>
               </div>
 
-              <div className="absolute left-6 bottom-6 w-28 h-28 md:w-36 md:h-36 transform rotate-2">
-                <HeroAnimalCard src={images.cow2} alt="Cow 2" />
+              {/* Bottom Left - Sheep (Even Bigger) */}
+              <div className="absolute -left-24 -bottom-16 w-48 h-48 md:w-60 md:h-60 transform rotate-2">
+                <HeroAnimalCard src={images.sheep} alt="Sheep" />
               </div>
             </div>
           </div>
