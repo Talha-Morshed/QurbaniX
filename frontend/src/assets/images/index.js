5ff0d5ef1f7745602c8c36848d068bd1fe943a49
Talha-Morshed <professionalxtalha@gmail.com>
2026-08-15 20:24:47 +0600

    Added logo to home page

--- frontend/src/assets/images/index.js
+++ frontend/src/assets/images/index.js
@@ -3,12 +3,14 @@ import cow from './cow.png';
 import goat from './goat.png';
 import camel from './camel.png';
 import sheep from './sheep.png';
+import logo from './logo.png';
 
 const images = {
   cow,
   goat,
   camel,
   sheep,
+  logo,
 };
 
 export default images;
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,10 +1,11 @@
 import './Header.css';
+import images from '../assets/images';
 
 function Header() {
   return (
     <header className="header">
       <a className="header__brand" href="#home">
-        QurbaniX
+        <img src={images.logo} alt="QurbaniX Logo" className="header__logo" />
       </a>
       <nav className="header__nav">
         <a href="#home">Home</a>
